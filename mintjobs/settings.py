6e43fd5368af300afe18acb089761fa4f11ea2a8
Timothy Ojo <66871563+Timmyojo@users.noreply.github.com>
2024-08-31 16:12:41 +0000

    added miration

--- mintjobs/settings.py
+++ mintjobs/settings.py
@@ -23,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'ghhfghjj345565345555'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+#DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'core.User'
 
@@ -96,7 +99,7 @@ DATABASES = {
 
 database_url = os.environ.get('DATABASE_URL')
 
-DATABASES['default'] = dj_database_url.parse(database_url)
+DATABASES['default'] = dj_database_url.parse('postgresql://mintjobsdb_user:uYW49JpJirVnbGQbIg1Hd8mknEDjBb25@dpg-cr9ftu56l47c73cmg5a0-a.oregon-postgres.render.com/mintjobsdb')
 
 
 # Password validation
